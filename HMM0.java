77c8de6572d415f9a5bf01750c78f9f422aec994
fannyradesjo <test@test.com>
2018-09-07 15:28:26 +0200

    fler filer

---
+++ HMM0.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class HMM0 {
+      public static void main(String[] args) {
+      Scanner scan = new Scanner(System.in);
+      String str = scan.nextLine();
+      String[] data = str.split(" ");
+      System.out.println(Arrays.toString(data));
+      }
+
+}
