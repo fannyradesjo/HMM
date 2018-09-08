4be4840db192411697f33ce6c98256ebe9287471
fannyradesjo <test@test.com>
2018-09-08 14:50:38 +0200

    input fixat

--- HMM0.java
+++ HMM0.java
@@ -1,11 +1,35 @@
 import java.util.*;
+import java.io.IOException;
 
 public class HMM0 {
-      public static void main(String[] args) {
-      Scanner scan = new Scanner(System.in);
-      String str = scan.nextLine();
-      String[] data = str.split(" ");
-      System.out.println(Arrays.toString(data));
+
+      public static String[] takeInput(){
+        Scanner scan = new Scanner(System.in);
+        List<String> data = new ArrayList<String>();
+        String Line;
+
+        while(scan.hasNext()){
+          Line = scan.nextLine();
+          String[] LineArray = Line.split(" ");
+
+          for(String e: Line.split(" ")){
+              data.add(e);
+          }
+
+        }
+
+      String[] dataArray = new String[data.size()];
+        for(int i = 0; i < data.size(); i++){
+          dataArray[i] = data.get(i);
       }
+      return dataArray;
+      }
+
+      public static void main(String[] args) {
+
+      String[] data = takeInput();
+
+      System.out.println(Arrays.toString(data));
+    }
 
 }
