03b9ec3b7e16221d9579acc7ac0a88a8ffab2ac5
fannyradesjo <test@test.com>
2018-09-09 13:47:53 +0200

    Den funkar nu

--- HMM0.java
+++ HMM0.java
@@ -1,5 +1,6 @@
 import java.util.*;
 import java.lang.Math;
+import java.lang.StringBuilder;
 
 public class HMM0 {
 
@@ -16,6 +17,37 @@ public class HMM0 {
         return data;
       }
 
+      public static Float[][] matrixMult(Float[][] a, Float[][] b){
+        int m1 = a.length;
+        int n1 = a[0].length;
+        int m2 = b.length;
+        int n2 = b[0].length;
+        if (n1 != m2) throw new RuntimeException("Illegal matrix dimensions.");
+        Float[][] c = new Float[m1][n2];
+        for (int i = 0; i < m1; i++){
+          for (int j = 0; j < n2; j++){
+            c[i][j] = 0f;
+            for (int k = 0; k < n1; k++){
+            c[i][j] += a[i][k] * b[k][j];}}}
+            return c;
+      }
+
+
+      public static String fixOutput(Float[][] ans){
+        StringBuilder builder = new StringBuilder();
+        builder.append("1");
+        builder.append(" ");
+        builder.append(Integer.toString(ans[0].length));
+        for(Float e: ans[0]) {
+          builder.append(" ");
+          builder.append(Float.toString(e));
+        }
+        //builder.setLength(builder.length() - 1);
+        String str = builder.toString();
+        return str;
+
+  }
+
 
 
       public static void main(String[] args) {
@@ -27,7 +59,7 @@ public class HMM0 {
       int bcol =Integer.parseInt(data[1][1]);
       Float[][] A = new Float[arow][acol];
       Float[][] B = new Float[brow][bcol];
-      Float[] pi = new Float[data[2].length-2];
+      Float[][] pi = new Float[1][data[2].length-2];
 
       int w = 2;
       for(int i = 0; i < arow; i++){
@@ -46,9 +78,10 @@ public class HMM0 {
       }
 
       for(int i = 0; i < Integer.parseInt(data[2][1]); i++){
-        pi[i] = Float.parseFloat(data[2][i+2]);
+        pi[0][i] = Float.parseFloat(data[2][i+2]);
       }
 
+      /*
       System.out.println("A: ");
       for(int i = 0; i < A.length; i++){
       System.out.println(Arrays.toString(A[i]));}
@@ -56,7 +89,23 @@ public class HMM0 {
       for(int i = 0; i < B.length; i++){
       System.out.println(Arrays.toString(B[i]));}
       System.out.println("pi: ");
-      System.out.println(Arrays.toString(pi));
+      System.out.println(Arrays.toString(pi[0]));*/
+
+      Float[][] C = matrixMult(pi, A);
+      Float[][] ans = matrixMult(C, B);
+
+      /*
+      System.out.println("C: ");
+      for(int i = 0; i < C.length; i++){
+      System.out.println(Arrays.toString(C[i]));}
+      System.out.println("ans: ");
+      for(int i = 0; i < ans.length; i++){
+      System.out.println(Arrays.toString(ans[i]));}*/
+
+      String output = fixOutput(ans);
+      System.out.println(output);
+
+
 
     }
 
